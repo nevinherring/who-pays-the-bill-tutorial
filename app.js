83bb117facf1861c54e7fd92c1835893972b50cf
Nevin Herring <nevin.herring@gmail.com>
2018-11-21 06:45:10 -0600

    form validation

--- app.js
+++ app.js
@@ -7,18 +7,20 @@
     }
 
     this.addApplicants = function(){
-        var addBtn = document.querySelector("#add_applicant");
 
         function generateList(input){
 
             var value = input.value;
-
-            applicants.push(value);
+            if(this.checkValid(value.toLowerCase())){
+            applicants.push(value.toLowerCase());
             input.value = '';
-
+            } else {
+                alert("no duplicate names, please list only unique names");
+            }
             console.log(applicants);
         }
 
+        var addBtn = document.querySelector("#add_applicant");
         addBtn.addEventListener("click", function(){
 
             var input = document.querySelector("#applicant_value");
@@ -27,6 +29,15 @@
         });
     }
 
+    this.checkValid = function(value){
+        
+        if(applicants.indexOf(value) < 0  && value !== ''){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
 
 
     this.init();
