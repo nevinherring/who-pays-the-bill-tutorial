ab97d36f49470e3e9082f5270b57c5b98f88cca4
Nevin Herring <nevin.herring@gmail.com>
2018-11-22 11:04:17 -0600

    finished js

--- app.js
+++ app.js
@@ -4,6 +4,23 @@
 
     this.init = function(){
         this.addApplicants();
+        this.getRandomUser();
+        this.runAgain();
+        this.startOver();
+    };
+
+    this.showList = function(){
+
+        var parent = document.querySelector(".applicant_list_wrapper");
+        var template = '';
+
+        for(var i = 0; i < applicants.length; i++){
+            template += '<span class="name-tag" data-id="'+ i +'">' + applicants[i] + '</span>'
+        }
+
+        parent.innerHTML = '';
+        parent.insertAdjacentHTML('afterbegin', template);
+        deleteOne();
     }
 
     this.addApplicants = function(){
@@ -14,10 +31,11 @@
             if(this.checkValid(value.toLowerCase())){
             applicants.push(value.toLowerCase());
             input.value = '';
+            showList();
             } else {
                 alert("no duplicate names, please list only unique names");
             }
-            console.log(applicants);
+            
         }
 
         var addBtn = document.querySelector("#add_applicant");
@@ -39,6 +57,78 @@
         
     }
 
+    this.deleteOne = function(){
+
+        var item = document.querySelectorAll('.name-tag');
+
+        function removeIt(element){
+            var attr = parseInt(element.getAttribute('data-id'));
+
+            applicants.splice(attr, 1);
+            this.showList();
+        }
+
+
+        for(var i = 0; i < item.length; i++){
+            item[i].addEventListener('click', function(){
+                removeIt(this);
+            });
+        }
+
+    };
+
+    this.getRandomUser = function(){
+        var resultsButton = document.querySelector('#show_results');
+        
+        function showLooser(){
+            var resultsContainer = document.querySelector('.results_container');
+            var applicantsContainer = document.querySelector('.applicant_container');
+
+            applicantsContainer.className += ' hidden';
+            resultsContainer.className = 'results_container';
+
+            showRandomUser();
+        }
+        
+        resultsButton.addEventListener('click', function(e){
+            if(applicants.length > 1){
+                showLooser();
+            }
+        });
+    }
+
+    this.showRandomUser = function(){
+
+        var resultContainer = document.querySelector('.result');
+        var rand = applicants[Math.floor(Math.random() * applicants.length)];
+
+        resultContainer.innerHTML = '';
+        resultContainer.insertAdjacentHTML('afterbegin', '<h3>' + rand + '</h3>');
+
+    }
+
+    this.runAgain = function(){
+        
+        runAgainBtn = document.querySelector('.run_again');
+        runAgainBtn.addEventListener("click", function(e){
+            showRandomUser();
+        });
+    }
+
+    this.startOver = function(){
+        var startAgainBtn = document.querySelector('.start_again');
+
+        startAgainBtn.addEventListener('click', function(){
+            var applicantsContainer = document.querySelector('.applicant_caontainer');
+            var applicantsWrapper = docuement.querySelector('.applicants_list_wrapper');
+
+            applicantsContainer.className = 'applicant_container';
+            resultsContainer.className = 'results_container hidden';
+            applicantsWrapper.innerHTML = '';
+
+            applicants = [];
+        });
+    }
 
     this.init();
 })();
